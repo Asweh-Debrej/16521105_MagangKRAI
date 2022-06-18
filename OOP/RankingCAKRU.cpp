ddb0271e5d728da63e9201b79af78a0551b43f21
Asweh Debrej <aswehdebrej@gmail.com>
2022-06-18 10:57:47 +0700

    file baru
    menambah isi file (masi error)

--- OOP/RankingCAKRU.cpp
+++ OOP/RankingCAKRU.cpp
@@ -2,3 +2,54 @@
 
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+int RankingCAKRU::jumlahCakru = 0;
+
+RankingCAKRU::RankingCAKRU() {
+    CAKRU cakru;
+    this->namaLeaderBoard = "Unnamed";
+    this->listCakru = new CAKRU[MAX_CAKRU];
+}
+
+RankingCAKRU::RankingCAKRU(const RankingCAKRU& panutan) {
+    cout << "Leaderboard " << this->namaLeaderBoard << " telah disalin." << endl;
+    this->namaLeaderBoard = panutan.namaLeaderBoard;
+    this->listCakru = panutan.listCakru;
+}
+
+RankingCAKRU::~RankingCAKRU() {
+    delete[] this->listCakru;
+}
+
+void RankingCAKRU::add(CAKRU &cakru) {
+    for(int i = 0; i < MAX_CAKRU; i++) {
+        if(this->listCakru[i].getNIM() == cakru.getNIM()) {
+            cout << "Cakru " << this->listCakru[i].getNama() << " gagal ditambahkan ke leaderboard karena NIM masih nol atau duplikat." << endl;
+            return;
+        }
+    }
+}
+
+void RankingCAKRU::remove(int nim) {
+    cout << "CAKRU " << this->find(nim).getNama() << " telah dikeluarkan dari leaderboard " << this->namaLeaderBoard << "." << endl;
+    this->jumlahCakru --;
+    delete &this->find(nim);
+}
+
+void RankingCAKRU::show() {
+    for(int i = 0; i < this->jumlahCakru; i++) {
+        this->listCakru[i].tampil();
+    }
+}
+
+CAKRU &RankingCAKRU::find(int nim) {
+    CAKRU hasil;
+    for(int i = 0; i < this->jumlahCakru; i++) {
+        if (this->listCakru[i].getNIM() == nim) {
+            return this->listCakru[i];
+        }
+    }
+    return hasil;
+}
--- OOP/RankingCAKRU.hpp
+++ OOP/RankingCAKRU.hpp
@@ -1,10 +1,28 @@
-#ifndef _CAKRU_HPP
-#define _CAKRU_HPP
+#ifndef _RankingCAKRU_HPP
+#define _RankingCAKRU_HPP
 
 #include <iostream>
 #include <string>
+#include "CAKRU.cpp"
+
+#define MAX_CAKRU 10
 
 class RankingCAKRU {
+    private:
+        string namaLeaderBoard;
+        static int jumlahCakru;
+        CAKRU *listCakru;
+
+    public:
+        RankingCAKRU();
+        RankingCAKRU(const RankingCAKRU&);
+        ~RankingCAKRU();
+
+        void add(CAKRU &cakru);
+        void remove(int nim);
+        void show();
+        CAKRU find(int nim);
+
 
 };
 
